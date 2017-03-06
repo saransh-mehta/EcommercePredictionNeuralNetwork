3ff718dbe9f8b0438e1dd5a5af5b9b74efb8da1e
Saransh Mehta <mehtasaransh614@gmail.com>
2017-03-07 00:21:35 +0530

    Update airlineData.py
    Adding some coments

--- airlineData.py
+++ airlineData.py
@@ -5,7 +5,11 @@ f=pd.read_csv("F:\PYTHON PROGRAMS\gitClone\machine_learning_examples-master\airl
 
 print(f.coloumns)
 
+#renaming the coloumns as the name is not good
+
 f.coloumns= ["months","passengers"]
+
+#adding an extra coloumns with only 1's
 
 f['ones']=1
 print(f.head())
